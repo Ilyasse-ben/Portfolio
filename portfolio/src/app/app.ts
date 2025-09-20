24c9bb06ae946ee4a1e40b131dc488b98f488031
Ilyasse-ben <ilyassebentaleb7@gmail.com>
2025-09-20 16:41:00 +0100

    skilse & contactue  tasks

--- portfolio/src/app/app.ts
+++ portfolio/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +7,12 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit{
   protected readonly title = signal('portfolio');
+  ngOnInit(): void {
+
+  }
+  afficheDiv(div1:HTMLDivElement,div2:HTMLDivElement,div3:HTMLDivElement){
+    div1.toggleAttribute('Hidden')
+  }
 }
